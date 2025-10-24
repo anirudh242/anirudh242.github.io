90d273f9f310c0a4a7c2e0408d77413064a00402
Anirudh Bhardwaj <73819495+anirudh242@users.noreply.github.com>
2025-10-24 15:52:37 +0530

    Add basePath and assetPrefix for production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
+const repoName = 'portfolio-new';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
